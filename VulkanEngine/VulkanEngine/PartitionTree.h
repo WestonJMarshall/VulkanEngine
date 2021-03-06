2614fd3075bd965d2a94e620e88da379b73a7aad
wjm3824 <wjm3824@rit.edu>
2021-03-06 12:14:05 -0700

    Partial integration with physics system

---
+++ VulkanEngine/VulkanEngine/PartitionTree.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "pch.h"
+
+class PartitionTree
+{
+public:
+	virtual int Initialize(int maxSubdivisions, int minToSubdivide) = 0;
+	virtual int Fill(std::shared_ptr<GameObject> gameObject) = 0;
+	virtual int Generate() = 0;
+
+	virtual int Draw() = 0;
+
+	std::vector<std::weak_ptr<GameObject>> GetObjects()
+	{
+		return objects;
+	}
+
+protected:
+	std::vector<std::weak_ptr<GameObject>> objects;
+};
+
